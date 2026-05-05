525e30f01fb86a6e95895c7a1ae6a0a32f2b230c
Lorem-INC <jafthab05@gmail.com>
2026-05-05 21:12:05 +0530

    Use environment variable for server port

--- joblogger/server.js
+++ joblogger/server.js
@@ -11,8 +11,11 @@ const { getVTCMembers } = require("./Truckermpapi.js");
 const PUBLIC_DIR = path.join(__dirname, "..");
 app.use(express.static(PUBLIC_DIR));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 // ==============================
 // CONFIG
 // ==============================
